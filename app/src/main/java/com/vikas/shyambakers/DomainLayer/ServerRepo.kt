6523e9b610c9fd6987807d4e8bb714641dd9179e
skaur16 <me.sharandeepkaur16@gmail.com>
2024-03-04 22:57:19 +0530

    ServerRepo created..

--- app/src/main/java/com/vikas/shyambakers/DomainLayer/ServerRepo.kt
+++ app/src/main/java/com/vikas/shyambakers/DomainLayer/ServerRepo.kt
@@ -1,8 +1,26 @@
 package com.vikas.shyambakers.DomainLayer
 
+import com.google.type.Date
+import com.vikas.shyambakers.DataLayer.Bill
 import com.vikas.shyambakers.DataLayer.Product
+import java.time.Month
 
 interface ServerRepo {
 
-    suspend fun sendInventory(product: Product)
+    fun addInventoryProduct(product : Product)
+    suspend fun getInventoryProducts() : List<Product>
+
+    fun addSaleBill(bill : Bill, month: Month)
+
+    suspend fun getSaleBills(month: Month, date: Date) : List<Bill>
+
+    suspend fun getTotalSales(month : Month) : Int
+
+    fun addPurchaseBill(bill : Bill, month : Month)
+    suspend fun getPurchaseBills(month: Month, date: Date) : List<Bill>
+
+    suspend fun getTotalPurchase(month:Month) : Int
+    fun calculateProfit(totalSales : Int, totalPurchase : Int, otherExpense : Int) : Int
+    fun maxSoldProduct(month : Month) : Product
+
 }
